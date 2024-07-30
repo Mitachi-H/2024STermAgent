8a595019aa2dff519d43ef58e6857486528713e8
tsunekazuomija <tsunekazuomija@g.ecc.u-tokyo.ac.jp>
2024-07-30 09:44:45 +0900

    自分のPredictionRoleには初期値を設定

--- AIWolfNLAgentPython/player2024s/predict_role.py
+++ AIWolfNLAgentPython/player2024s/predict_role.py
@@ -13,7 +13,13 @@ class Predictions():
         self.my_agent_id: str = my_agent_id
         self.my_agent_role: str = my_agent_role
         self.roleNumMap: Dict[str, int] = roleNumMap
-        self.predict_roles: list[PredictionRole] = [PredictionRole(agent_id=agent_id, alive=True) for agent_id in statusMap.keys()]
+
+        self.predict_roles: list[PredictionRole] = []
+        for agent_id in statusMap.keys():
+            if int(agent_id) == int(my_agent_id):
+                self.predict_roles.append(PredictionRole(agent_id=agent_id, alive=True, role=my_agent_role, reason="自分の役職は確定している"))
+            else:
+                self.predict_roles.append(PredictionRole(agent_id=agent_id, alive=True, reason="役職の予想に必要な情報が不足している"))
         # self.reasons: str = None
     
     def update_alive(self, alive: list[int]):
